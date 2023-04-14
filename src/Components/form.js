1d42b0656fc6e7229a0ed50f926be76fe935c8c9
pharesha <gio.faresha@gmail.com>
2023-04-14 20:00:40 +0400

    fixed input and buttons & added api inegration of random memes

--- src/Components/form.js
+++ src/Components/form.js
@@ -1,14 +1,28 @@
 import React from "react";
+import memesData from "./memesData";
 
-const Form = () => {
+export default function Form (){
+    
+    const [memeImage, setMemeImage] = React.useState("");
+
+
+
+    function generateMeme(){
+        const memeUrl = memesData.data.memes[Math.floor(Math.random() * memesData.data.memes.length)].url;
+        setMemeImage(memeUrl)
+        console.log(memeUrl);
+    }
+
     return(
         <div className="form-div">
-            <form>
+            <div className="form">
                 <input type="text" placeholder="Top Text" className="top-text"/>
                 <input type="text" placeholder="Bottom Text" className="bottom-text"/>
-                <button>Generate a New Meme</button>
-            </form>
+                <button onClick={generateMeme}>Generate a New Meme</button>
+            </div>
+            <div className="image-div">
+                <img src={memeImage} alt="meme" className="image"/>
+            </div>
         </div>
     );
 }
-export default Form;
--- src/Components/header.js
+++ src/Components/header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const icon = "https://upload.wikimedia.org/wikipedia/en/9/96/Meme_Man_on_transparent_background.webp";
 
-const header = () =>{
+export default function Header() {
     return(
         <div className="header-div">
             <header>
@@ -12,5 +12,3 @@ const header = () =>{
         </div>
     );
 }
-
-export default header;
